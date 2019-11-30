43ed7c4b9dc758641b20f5429212b4143d835c2a
Tuncay ÇOLAK <tuncay.colak@tubitak.gov.tr>
2019-11-30 14:31:03 +0300

    get display number for gnome and set display notify task

--- src/plugins/notify/eta_notify.py
+++ src/plugins/notify/eta_notify.py
@@ -4,31 +4,39 @@
 
 from base.plugin.abstract_plugin import AbstractPlugin
 from base.model.enum.content_type import ContentType
-import json
 
-class Sample(AbstractPlugin):
+class Notify(AbstractPlugin):
     def __init__(self, task, context):
-        super(Sample, self).__init__()
+        super(Notify, self).__init__()
         self.task = task
         self.message_code = self.get_message_code()
         self.context = context
         self.logger = self.get_logger()
-        self.eta_notify_command_fullscreen = "DISPLAY=:0 eta-notify -m  \"{message_content}\" -d {duration}"
-        self.eta_notify_command_small = "DISPLAY=:0 eta-notify -m  \"{message_content}\" -d {duration} --{size}"
+        self.eta_notify_command_fullscreen = "su - {0} -c 'DISPLAY={1} eta-notify -m  \"{2}\" -d {3}'"
+        self.eta_notify_command_small = "su - {0} -c 'DISPLAY={1} eta-notify -m  \"{2}\" -d {3} --{4}'"
         self.message_content = None
 
     def handle_task(self):
-        try:
-            self.message_content = self.task['notify_content']
-            message_duration = self.task['duration']
-            message_size = self.task['size']
-            if self.task['size'] == 'small':
-                result_code, p_out, p_err = self.execute(self.eta_notify_command_small.format(message_content=self.message_content, duration=message_duration, size=message_size))
-                self.create_response(result_code, p_err)
 
-            else:
-                result_code, p_out, p_err = self.execute(self.eta_notify_command_fullscreen.format(message_content=self.message_content, duration=message_duration))
-                self.create_response(result_code, p_err)
+        result_code = None
+        p_err = None
+        try:
+            users = self.Sessions.user_name()
+            for username in users:
+                self.logger.debug("username: " + str(username))
+                display_number = self.get_username_display_gnome(username)
+                self.logger.debug("get display number : " + str(display_number))
+                self.message_content = self.task['notify_content']
+                message_duration = self.task['duration']
+                message_size = self.task['size']
+                if self.task['size'] == 'small':
+                    result_code, p_out, p_err = self.execute(self.eta_notify_command_small.format(username, display_number, self.message_content, message_duration, message_size))
+                    # self.create_response(result_code, p_err)
+                else:
+                    result_code, p_out, p_err = self.execute(self.eta_notify_command_fullscreen.format(username, display_number, self.message_content, message_duration))
+                    # self.create_response(result_code, p_err)
+
+            self.create_response(result_code, p_err)
 
         except Exception as e:
             self.logger.error("*********"+str(e))
@@ -58,11 +66,12 @@ class Sample(AbstractPlugin):
             status = False
 
         cols = ['send_time', 'content', 'duration', 'status']
-        values = [self.task["send_time"], self.task["notify_content"], self.task["duration"],status]
+        values = [self.task["send_time"], self.task["notify_content"], self.task["duration"], status]
         self.db_service.update('notify', cols, values)
         self.logger.debug("Saved notify to ahenk.db")
 
+
 def handle_task(task, context):
     print('Sample Plugin Task')
-    sample = Sample(task, context)
-    sample.handle_task()
+    app = Notify(task, context)
+    app.handle_task()
